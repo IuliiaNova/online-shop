59bd64c39581c9a4eb1986c31f288d1b7bc3afb9
Iuliia Shikhanova <yshihanova@gmail.com>
2023-03-02 15:31:40 +0000

    create{base}: base header, footer + base style

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const Footer = () => {
+  return (
+    <footer>
+        &copy; All rights are protected
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+ const  Header = () => {
+  return (
+    <header className=''>
+        <div>
+            <span className='logo'>Simple care</span>
+            <ul className='nav'>
+                <li>Home</li>
+                <li>About us</li>
+            </ul>
+        </div>
+        <div className='presentation'>
+
+        </div>
+    </header>
+  )
+}
+
+export default Header;
